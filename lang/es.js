0d44bf5422a2f1249ed8046ba4b337bb0010abbf
Marc Espín Sanz <mespinsanz@gmail.com>
2021-10-17 00:20:27 +0200

    :flag_es: Translated bot to Spanish

---
+++ lang/es.js
@@ -0,0 +1,73 @@
+export const emojis = [
+    {
+      id: 'spookybot',
+      emoji: '761602615326146590',
+      words: ['spooky'],
+    },
+    {
+      id: 'bat',
+      emoji: '🦇',
+      words: ['muerciélago'],
+    },
+    {
+      id: 'bone',
+      emoji: '🦴',
+      words: ['hueso'],
+    },
+    {
+      id: 'brain',
+      emoji: '🧠',
+      words: ['cerebro'],
+    },
+    {
+      id: 'broom',
+      emoji: '🧹',
+      words: ['escoba'],
+    },
+    {
+      id: 'candy',
+      emoji: '🍬',
+      words: ['chuche','dulce'],
+    },
+    {
+      id: 'ghost',
+      emoji: '👻',
+      words: ['fantasma'],
+    },
+    {
+      id: 'lollipop',
+      emoji: '🍭',
+      words: ['piruleta'],
+    },
+    {
+      id: 'pumpkin',
+      emoji: '🎃',
+      words: ['halloween', 'calabaza'],
+    },
+    {
+      id: 'skull',
+      emoji: '💀',
+      words: ['cráneo'],
+    },
+    {
+      id: 'vampire',
+      emoji: '🧛',
+      words: ['vampiro'],
+    },
+    {
+      id: 'witch',
+      emoji: '🧙‍♀️',
+      words: ['hechizero', 'mago'],
+    },
+    {
+      id: 'zombie',
+      emoji: '🧟‍♂️',
+      words: ['zombie'],
+    },
+    {
+      id: 'alien',
+      emoji: '👽',
+      words: ['monstruo', 'alien'],
+    },
+  ]
+  
